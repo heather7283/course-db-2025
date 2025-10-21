b8f01d85489afcff947a6d31c6165ab966bfca5f
heather7283 <heather7283@protonmail.com>
2025-10-21 18:33:44 +0400

    add some more test queries

--- test_queries.sql
+++ test_queries.sql
@@ -19,3 +19,26 @@ JOIN countries c ON a.country_code = c.code
 WHERE a.name = 'Алексей Козлов'
 ORDER BY comp.time;
 
+-- инфа про команды и кол-во участников в команде
+SELECT
+    t.name AS team,
+    c.name AS country,
+    s.name AS sport,
+    COUNT(*) AS member_count
+FROM teams t
+JOIN countries c ON t.country_code = c.code
+JOIN sports s ON t.sport_code = s.code
+JOIN team_members tm ON tm.athlete_id = t.id
+GROUP BY tm.team_id;
+
+-- узнать какие соревнования и где проводятся в определённый день
+SELECT
+    s.name AS sport,
+    st.name AS site,
+    c.time AS time
+FROM competitions c
+JOIN sports s ON c.sport_code = s.code
+JOIN sites st ON c.site_id = st.id
+WHERE date(c.time) = '2024-01-15';
+
+
